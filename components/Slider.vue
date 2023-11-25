17a195840bfac211da0df4f14b0a10dced5de393
Christopher Santana <christopher@millionairepub.com>
2023-11-25 17:57:03 -0400

    Refactored slider to vue component

--- components/Slider.vue
+++ components/Slider.vue
@@ -2,170 +2,55 @@
   <section id="portfolio-container" class="section is-large">
     <div class="glide glide--ltr glide--slider glide--swipeable">
       <div class="glide__track" data-glide-el="track">
-        <ul
-          class="glide__slides"
-          style="
-            transition: transform 0ms cubic-bezier(0.165, 0.84, 0.44, 1) 0s;
-            width: 2218px;
-            transform: translate3d(0px, 0px, 0px);
-          "
-        >
-          <li
-            class="glide__slide glide__slide--active"
-            style="width: 517px; margin-right: 25px"
-          >
-            <div class="portfolio-el">
-              <a
-                target="_self"
-                class="portfolio-el-img"
-                draggable="true"
-                data-href="http://chsantana.com/orca"
-                href="http://chsantana.com/orca"
-              >
-                <img
-                  src="http://chsantana.com/img/orca/orca-preview.png"
-                  alt=""
-                  style="cursor: zoom-in"
-              /></a>
-
-              <div class="portfolio-el-details has-text-centered">
-                <h3 class="is-3">ORCA</h3>
-                <p>
-                  Work in progress timezones management web app for diverse
-                  teams.
-                </p>
-                <a
-                  class="button is-outlined"
-                  draggable="true"
-                  data-href="http://chsantana.com/orca"
-                  href="http://chsantana.com/orca"
-                  >View case</a
-                >
-              </div>
-            </div>
-          </li>
+        <ul class="glide__slides" ref="slider">
           <li
             class="glide__slide"
-            style="width: 517px; margin-left: 25px; margin-right: 25px"
+            v-for="(item, index) in portfolio_data"
+            :data-slider-index="index"
           >
             <div class="portfolio-el">
               <a
                 target="_self"
                 class="portfolio-el-img"
                 draggable="true"
-                data-href="http://chsantana.com/playsplit"
-                href="http://chsantana.com/playsplit"
+                :data-href="item.url"
+                :href="item.url"
               >
-                <img
-                  src="http://chsantana.com/img/playsplit/preview.png"
-                  alt=""
-                  style="cursor: zoom-in"
+                <img :src="item.thumbnail" alt="" style="cursor: zoom-in"
               /></a>
 
               <div class="portfolio-el-details has-text-centered">
-                <h3 class="is-3">Playsplit</h3>
-                <p>Split, merge, and/or reorganize your Spotify playlists.</p>
-                <a
-                  class="button is-outlined"
-                  draggable="true"
-                  data-href="http://chsantana.com/playsplit"
-                  href="http://chsantana.com/playsplit"
-                  >View case</a
-                >
-              </div>
-            </div>
-          </li>
-          <li
-            class="glide__slide"
-            style="width: 517px; margin-left: 25px; margin-right: 25px"
-          >
-            <div class="portfolio-el">
-              <a
-                target="_self"
-                class="portfolio-el-img"
-                draggable="true"
-                data-href="http://chsantana.com/chimio"
-                href="http://chsantana.com/chimio"
-              >
-                <img
-                  src="http://chsantana.com/img/chimio-img/chimio-preview-1.png"
-                  alt=""
-                  style="cursor: zoom-in"
-              /></a>
-
-              <div class="portfolio-el-details has-text-centered">
-                <h3 class="is-3">Chimio</h3>
+                <h3 class="is-3">{{ item.project_name }}</h3>
                 <p>
-                  Anonymous secret sharing app for people seeking fun, to kill
-                  time or relief.
+                  {{ item.description }}
                 </p>
                 <a
                   class="button is-outlined"
                   draggable="true"
-                  data-href="http://chsantana.com/chimio"
-                  href="http://chsantana.com/chimio"
+                  :data-href="item.url"
+                  :href="item.url"
                   >View case</a
                 >
               </div>
             </div>
           </li>
-
-          <li class="glide__slide" style="width: 517px; margin-left: 25px">
-            <div class="portfolio-el">
-              <a
-                class="portfolio-el-img"
-                target="_self"
-                draggable="true"
-                data-href="http://chsantana.com/fueltrack"
-                href="http://chsantana.com/fueltrack"
-              >
-                <img
-                  src="http://chsantana.com/img/fueltrack.og.png"
-                  alt=""
-                  style="cursor: zoom-in"
-              /></a>
-
-              <div class="portfolio-el-details has-text-centered">
-                <h3 class="is-3">Fueltrack</h3>
-                <p>
-                  Web and mobile app to track and manage vehicle maintenance and
-                  fuel expenses.
-                </p>
-                <a
-                  class="button is-outlined"
-                  draggable="true"
-                  data-href="http://chsantana.com/fueltrack"
-                  href="http://chsantana.com/fueltrack"
-                  >View case</a
-                >
-              </div>
-            </div>
-          </li>
-
-          <!-- 
-                    <li class="glide__slide" style="width: 570.909px; margin-left: 50px;">
-                        <div class="portfolio-el underconstruction">
-
-                            <h3 class="is-3">CARSANT CONSULTING</h3>
-
-                            <p><b>Website Design.</b></p> <p>Case study in progress...</p>
-
-                        </div>
-
-                    </li> -->
         </ul>
       </div>
 
       <div class="glide__arrows" data-glide-el="controls">
         <button
-          class="glide__arrow glide__arrow--left has-text-left-mobile is-disabled"
+          class="glide__arrow glide__arrow--left has-text-left-mobile"
+          :class="{ 'is-disabled': slideIndex === 0 }"
           data-glide-dir="<"
+          ref="prevBtn"
         >
           <i class="material-icons">arrow_back</i>prev
         </button>
         <button
           class="glide__arrow glide__arrow--right has-text-right-mobile"
+          :class="{ 'is-disabled': slideIndex === portfolio_data.length - 1 }"
           data-glide-dir=">"
+          ref="nextBtn"
         >
           next<i class="material-icons">arrow_forward</i>
         </button>
@@ -174,18 +59,25 @@
       <div id="slide-counter">
         <span
           data-slide-count="current"
+          ref="currentSlideCounter"
           class="has-stroke"
           style="transform: translateX(0px); opacity: 1"
           >1</span
         >
-        <span data-slide-count="total">4</span>
+        <span data-slide-count="total" ref="slideCounterTotal">{{
+          portfolio_data.length
+        }}</span>
         <span data-action="close" class="button is-hollow is-small"
           ><i class="material-icons"> close </i></span
         >
       </div>
 
       <div id="slide-scroll-bar">
-        <span id="slide-progress-bar" style="width: 25%; left: 0%"></span>
+        <span
+          id="slide-progress-bar"
+          ref="slideProgressBar"
+          :style="{ width: `${100 / portfolio_data.length}%`, left: '0%' }"
+        ></span>
       </div>
     </div>
   </section>
@@ -195,138 +87,92 @@
 import anime from "animejs";
 import Glide from "@glidejs/glide";
 
-// Assuming this code is inside a Vue Composition API setup function
+const slider = ref();
+const nextBtn = ref();
+const prevBtn = ref();
+const slideProgressBar = ref();
+const slideCounterTotal = ref();
+const currentSlideCounter = ref();
+const slideIndex = ref(0);
+const portfolio_data = reactive([
+  {
+    thumbnail: "http://chsantana.com/img/orca/orca-preview.png",
+    project_name: "ORCA",
+    description: `Work in progress timezones management web app for diverse teams.`,
+    url: "http://chsantana.com/orca",
+  },
+  {
+    thumbnail: "http://chsantana.com/img/chimio-img/chimio-preview-1.png",
+    project_name: "Chimio",
+    description: `Anonymous secret sharing app for people seeking fun, to kill time or relief.`,
+    url: "http://chsantana.com/chimio",
+  },
+  {
+    thumbnail: "http://chsantana.com/img/chimio-img/fueltrack.og.png",
+    project_name: "Fueltrack",
+    description: `Web and mobile app to track and manage vehicle maintenance and fuel expenses.`,
+    url: "http://chsantana.com/chimio",
+  },
+]);
+
+const sliderHandler = (glideIndex) => {
+  const slideList = document.querySelectorAll(".glide__slides li");
+  const currentEl = currentSlideCounter.value;
+  currentEl.innerHTML = 1;
+
+  slideIndex.value = glideIndex;
+
+  anime({
+    targets: currentEl,
+    duration: 250,
+    translateX: 30,
+    easing: "linear",
+    opacity: 0,
+  });
+
+  setTimeout(() => {
+    currentEl.innerHTML = glideIndex + 1;
+    currentEl.style.transform = "translateX(-30px)";
+
+    anime({
+      targets: currentEl,
+      duration: 250,
+      translateX: 0,
+      easing: "linear",
+      opacity: 1,
+    });
+  }, 250);
+
+  let slideWidth = 0;
+  if (glideIndex < slideList.length) {
+    slideWidth += (glideIndex / slideList.length) * 100;
+  } else {
+    slideWidth = 0;
+  }
+
+  anime({
+    targets: slideProgressBar.value,
+    duration: 500,
+    left: slideWidth + "%",
+    easing: "easeInOutCirc",
+  });
+};
+
 onMounted(() => {
   if (process.client) {
     const glide = new Glide(".glide");
     glide.mount();
 
-    const slideProgressBar = document.querySelector("#slide-progress-bar");
-    const slideContainer = document.querySelector('[data-glide-el="track"]');
-    const slideList = document.querySelectorAll(".glide__slides li");
-    const arrows = document.querySelector('[data-glide-el="controls"]');
-    const next = document.querySelector('[data-glide-dir=">"]');
-    const previews = document.querySelector('[data-glide-dir="<"]');
-    const currentEl = document.querySelector("[data-slide-count='current']");
-    currentEl.innerHTML = 1;
+    // const windowSize = screen.width;
+    // const sm = 768;
 
-    const slideCounterTotal = document.querySelector(
-      "[data-slide-count='total']"
-    );
-    slideCounterTotal.innerHTML = slideList.length;
+    // function deviseEvent() {
+    //   return windowSize > sm ? "click" : "touchend";
+    // }
 
-    const slideCounter = document.querySelector("#slide-counter");
-    const activeSlide = document.querySelector(".glide__slide--active");
-
-    const windowSize = screen.width;
-    const sm = 768;
-
-    function deviseEvent() {
-      return windowSize > sm ? "click" : "touchend";
-    }
-
-    next.addEventListener(deviseEvent(), () => {
-      const z1 = setInterval(() => {
-        if (document.querySelector(".glide__slide--active").index() > 0) {
-          clearInterval(z1);
-          previews.classList.remove("is-disabled");
-        }
-      }, 100);
-
-      const z2 = setInterval(() => {
-        if (
-          document.querySelector(".glide__slide--active").index() ===
-          slideList.length - 1
-        ) {
-          clearInterval(z2);
-          next.classList.add("is-disabled");
-        }
-      }, 100);
-
-      if (
-        document.querySelector(".glide__slide--active").index() ===
-        slideList.length - 1
-      ) {
-        const z3 = setInterval(() => {
-          if (document.querySelector(".glide__slide--active").index() === 0) {
-            next.classList.remove("is-disabled");
-            previews.classList.add("is-disabled");
-            clearInterval(z3);
-          }
-        }, 200);
-      }
-    });
-
-    previews.addEventListener(deviseEvent(), () => {
-      const z1 = setInterval(() => {
-        if (
-          document.querySelector(".glide__slide--active").index() ===
-          slideList.length - 1
-        ) {
-          clearInterval(z1);
-          previews.classList.remove("is-disabled");
-          next.classList.add("is-disabled");
-        }
-      }, 100);
-
-      const z2 = setInterval(() => {
-        if (document.querySelector(".glide__slide--active").index() === 0) {
-          clearInterval(z2);
-          previews.classList.add("is-disabled");
-        }
-      }, 100);
-
-      const z3 = setInterval(() => {
-        if (
-          document.querySelector(".glide__slide--active").index() <
-          slideList.length - 1
-        ) {
-          next.classList.remove("is-disabled");
-          clearInterval(z3);
-        }
-      }, 200);
-    });
-
-    glide.on("run", (e) => {
-      anime({
-        targets: currentEl,
-        duration: 250,
-        translateX: 30,
-        easing: "linear",
-        opacity: 0,
-      });
-
-      setTimeout(() => {
-        currentEl.innerHTML = glide.index + 1;
-        currentEl.style.transform = "translateX(-30px)";
-
-        anime({
-          targets: currentEl,
-          duration: 250,
-          translateX: 0,
-          easing: "linear",
-          opacity: 1,
-        });
-      }, 250);
-
-      let slideWidth = 0;
-      if (glide.index < slideList.length) {
-        slideWidth += (glide.index / slideList.length) * 100;
-      } else {
-        slideWidth = 0;
-      }
-
-      anime({
-        targets: slideProgressBar,
-        duration: 500,
-        left: slideWidth + "%",
-        easing: "easeInOutCirc",
-      });
-    });
+    glide.on("run", () => sliderHandler(glide.index));
 
     glide.mount();
-
-    // Other parts of your code depending on the context...
   }
 });
 </script>
